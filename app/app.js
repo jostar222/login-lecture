d46c1e333aba83b98ce3ac16f2e604c48c2db527
jostar222 <whtjdgus911@gmail.com>
2024-03-04 23:38:44 +0900

    프론트 단 javascript 만들기

--- app/app.js
+++ app/app.js
@@ -10,6 +10,8 @@ const home = require("./src/routes/home");
 //앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+//__dirname = app.js가 있는 폴더 위치 반환
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드.
 
