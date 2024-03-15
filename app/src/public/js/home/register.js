f6e88048e43f4a258f8dd39da2bebb9555da8f44
jostar222 <whtjdgus911@gmail.com>
2024-03-15 23:10:03 +0900

    fs모듈을 통한 회원가입 기능 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const id = document.querySelector("#id"),
+name = document.querySelector("#name"),
 psword = document.querySelector("#psword"),
 confirmPsword = document.querySelector("#confirm-psword"),
 registerBtn = document.querySelector("#button");
@@ -9,7 +10,7 @@ registerBtn.addEventListener("click", register);
 
 function register() {
     if (!id.value) return alert("아이디를 입력해주십시오.");
-    if (psword !== confirmPsword) return alert("비밀번호가 일치하지 않습니다.");
+    if (psword.value !== confirmPsword.value) return alert("비밀번호가 일치하지 않습니다.");
 
     const req = {
         id: id.value,
